7ecc36981e88ae1fc31bfb1ec4e393575a61652b
Scott Firestone <scott.firestone.dev@gmail.com>
2016-01-24 20:40:59 -0700

    Extension: Has colored backgrounds by guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,19 +6,33 @@ SECRET_NUMBER = rand(100)
 get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  background = check_background(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :background => background}
 end
 
+def check_background(guess)
+  if guess < SECRET_NUMBER - 5
+    :crimson
+  elsif guess > SECRET_NUMBER + 5
+    :crimson
+  elsif guess < SECRET_NUMBER
+    :lightcoral
+  elsif guess > SECRET_NUMBER
+    :lightcoral
+  else
+    :lawngreen
+  end
+end
 def check_guess(guess)
   if guess < SECRET_NUMBER - 5
-    message = "Way too low!\r\n\r\nThe SECRET NUMBER is #{SECRET_NUMBER}"
+    "Way too low!"
   elsif guess > SECRET_NUMBER + 5
-    message = "Way too high!\r\n\r\nThe SECRET NUMBER is #{SECRET_NUMBER}"
+    "Way too high!"
   elsif guess < SECRET_NUMBER
-    message = "Too low!\r\n\r\nThe SECRET NUMBER is #{SECRET_NUMBER}"
+    "Too low!"
   elsif guess > SECRET_NUMBER
-    message = "Too high!\r\n\r\nThe SECRET NUMBER is #{SECRET_NUMBER}"
+    "Too high!"
   else
-    message = "You got it right!\r\n\r\nThe SECRET NUMBER is #{SECRET_NUMBER}"
+    "You got it right!"
   end
 end
